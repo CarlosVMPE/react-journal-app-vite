206654141320e7bf185033753fbd73204c77090f
Carlos Vilchez Malasquez <carlosvmp0@gmail.com>
2022-07-14 23:27:47 -0500

    updating basename for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename='/react-journal-app-vite'>
         <JounalApp />
       </BrowserRouter>
     </Provider>
